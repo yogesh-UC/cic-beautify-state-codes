c0d5cd302daabbdc2734f58119f4d0354ddfef0a
glevita-uc <glevitad@unicourt.com>
2021-02-19 18:34:09 +0530

    added ky parser

--- ky_html_parser.py
+++ ky_html_parser.py
@@ -48,18 +48,21 @@ class KyHtmlOperations:
 
             elif header_tag.get("class") == [self.class_regex["sec_head"]]:
                 header_tag.name = "h3"
+                sec_pattern = re.compile(r'^(\d+\.\d+)')
+                if re.match(sec_pattern, header_tag.text.strip()):
+                    chap_num = re.search(r'^([^\.]+)', header_tag.text).group().zfill(2)
+                    sec_num = re.search(r'^(\d+\.\d+)', header_tag.text).group().zfill(2)
 
-                chap_num = re.search(r'^([^\.]+)', header_tag.text).group().zfill(2)
-                sec_num = re.search(r'^(\d+\.\d+)', header_tag.text).group(1).zfill(2)
-                header_pattern = re.search(r'^(\d+\.\d+)', header_tag.text.strip()).group()
-                if header_tag.find_previous(name="h3", class_=self.class_regex["sec_head"]) is not None:
-                    prev_tag = header_tag.find_previous(name="h3", class_=self.class_regex["sec_head"])
-                    if header_pattern in prev_tag.text:
-                        count = 0
-                        prev_tag["id"] = f"t{self.title_id}c{chap_num}s{sec_num}-{count + 1}"
-                        header_tag["id"] = f"t{self.title_id}c{chap_num}s{sec_num}-{count + 2}"
-                    else:
-                        header_tag["id"] = f"t{self.title_id}c{chap_num}s{sec_num}"
+                    header_pattern = re.search(r'^(\d+\.\d+)', header_tag.text.strip()).group()
+                    if header_tag.find_previous(name="h3", class_=self.class_regex["sec_head"]) is not None:
+
+                        prev_tag = header_tag.find_previous(name="h3", class_=self.class_regex["sec_head"])
+                        if header_pattern in prev_tag.text:
+                            count = 0
+                            prev_tag["id"] = f"t{self.title_id}c{chap_num}s{sec_num}-{count + 1}"
+                            header_tag["id"] = f"t{self.title_id}c{chap_num}s{sec_num}-{count + 2}"
+                        else:
+                            header_tag["id"] = f"t{self.title_id}c{chap_num}s{sec_num}"
 
             elif header_tag.get("class") == [self.class_regex["ul"]]:
                 header_tag.name = "li"
@@ -76,20 +79,6 @@ class KyHtmlOperations:
             else:
                 ul_tag = self.soup.new_tag("ul")
                 list_item.wrap(ul_tag)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     # wrap the main content
     def create_main_tag(self):
@@ -108,58 +97,59 @@ class KyHtmlOperations:
     # create a reference
     def create_chap_sec_nav(self):
         count = 0
-        for nav_tag in self.soup.find_all("li"):
-            if re.match(r'^(CHAPTER)', nav_tag.text.strip()):
-                chap_nav_nums = re.search(r'\d', nav_tag.text.strip())
-                chap_nums = re.search(r'\d', nav_tag.text).group(0).zfill(2)
+        for list_item in self.soup.find_all("li"):
+            if re.match(r'^(CHAPTER)', list_item.text.strip()):
+                chap_nav_nums = re.search(r'\d', list_item.text.strip())
+                chap_nums = re.search(r'\d', list_item.text).group(0).zfill(2)
                 if chap_nav_nums:
                     nav_list = []
                     nav_link = self.soup.new_tag('a')
-                    nav_link.append(nav_tag.text)
+                    nav_link.append(list_item.text)
                     nav_link["href"] = f"#t{self.title_id}c{chap_nums}"
                     nav_list.append(nav_link)
-                    nav_tag.contents = nav_list
+                    list_item.contents = nav_list
             else:
-                if re.match(r'^(\d+\.\d+)', nav_tag.text.strip()):
-                    chap_num = re.search(r'^([^\.]+)', nav_tag.text).group().zfill(2)
-                    sec_num = re.search(r'^(\d+\.\d+)', nav_tag.text).group(1).zfill(2)
-                    sec_pattern = re.search(r'^(\d+\.\d+)', nav_tag.text.strip()).group()
-                    sec_next_tag = nav_tag.find_next('li')
-                    # sec_prev_tag = nav_tag.find_previous("li")
-                    # sec_prev_tag_text = sec_prev_tag.text
+                if re.match(r'^(\d+\.\d+)', list_item.text.strip()):
+                    chap_num = re.search(r'^([^\.]+)', list_item.text).group().zfill(2)
+                    sec_num = re.search(r'^(\d+\.\d+)', list_item.text).group(1).zfill(2)
+                    sec_pattern = re.search(r'^(\d+\.\d+)', list_item.text.strip()).group()
+                    sec_next_tag = list_item.find_next('li')
+                    sec_prev_tag = list_item.find_previous("li")
+                    sec_prev_tag_text = sec_prev_tag.a
                     if sec_next_tag is not None:
                         if sec_pattern in sec_next_tag.text:
-                            nav_list = []
-                            nav_link = self.soup.new_tag('a')
-                            nav_link.string = nav_tag.text
-                            nav_link["href"] = f"#t{self.title_id}c{chap_num}s{sec_num}-{count + 1}"
-                            nav_list.append(nav_link)
-                            nav_tag.contents = nav_list
+                            new_list = []
+                            list_link = self.soup.new_tag('a')
+                            list_link.string = list_item.text
+                            list_link["href"] = f"#t{self.title_id}c{chap_num}s{sec_num}-{count + 1}"
+                            new_list.append(list_link)
+                            list_item.contents = new_list
 
-                        else:
-                            nav_list = []
-                            nav_link = self.soup.new_tag('a')
-                            nav_link.string = nav_tag.text
-                            nav_link["href"] = f"#t{self.title_id}c{chap_num}s{sec_num}"
-                            nav_list.append(nav_link)
-                            nav_tag.contents = nav_list
+                        elif sec_prev_tag_text is not None:
+                            if sec_pattern in sec_prev_tag.a.text:
+                                new_list = []
+                                list_link = self.soup.new_tag('a')
+                                list_link.string = list_item.text
+                                list_link["href"] = f"#t{self.title_id}c{chap_num}s{sec_num}-{count + 2}"
+                                new_list.append(list_link)
+                                list_item.contents = new_list
 
-                    else:
-                        nav_list = []
-                        nav_link = self.soup.new_tag('a')
-                        nav_link.string = nav_tag.text
-                        nav_link["href"] = f"#t{self.title_id}c{chap_num}s{sec_num}"
-                        nav_list.append(nav_link)
-                        nav_tag.contents = nav_list
+                            else:
+                                nav_list = []
+                                nav_link = self.soup.new_tag('a')
+                                nav_link.string = list_item.text
+                                nav_link["href"] = f"#t{self.title_id}c{chap_num}s{sec_num}"
+                                nav_list.append(nav_link)
+                                list_item.contents = nav_list
 
                 else:
-                    sec_id = re.sub(r'\s+', '', nav_tag.get_text()).lower()
+                    sec_id = re.sub(r'\s+', '', list_item.get_text()).lower()
                     new_list = []
                     new_link = self.soup.new_tag('a')
-                    new_link.string = nav_tag.text
+                    new_link.string = list_item.text
                     new_link["href"] = f"#t{self.title_id}c{chap_nums}{sec_id}"
                     new_list.append(new_link)
-                    nav_tag.contents = new_list
+                    list_item.contents = new_list
 
 
     # wrap a content with ol tag
@@ -246,7 +236,8 @@ class KyHtmlOperations:
                 tag.contents = []
                 tag.append(ol_tag3)
 
-            elif re.match(num_pattern, tag.text.strip()) and re.match(alphanum_pattern, tag.find_previous().text.strip()):
+            elif re.match(num_pattern, tag.text.strip()) and re.match(alphanum_pattern,
+                                                                      tag.find_previous().text.strip()):
                 ol_tag3.append(tag)
 
     # main method
@@ -258,11 +249,9 @@ class KyHtmlOperations:
         self.create_main_tag()
         self.set_appropriate_tag_name_and_id()
         self.create_ul_tag()
+
         self.create_chap_sec_nav()
 
-
-
-        # self.convert_li()
         # self.sec_headers()
         # self.main_tag()
         # self.ul_tag()
@@ -275,17 +264,19 @@ class KyHtmlOperations:
 
     # create a soup
     def create_soup(self):
-        with open("/home/mis/ky/gov.ky.krs.title.01.html") as fp:
+        with open("/home/mis/ky/gov.ky.krs.title.02.html") as fp:
             self.soup = BeautifulSoup(fp, "lxml")
 
     # write into a soup
     def write_into_soup(self):
-        with open("ky1.html", "w") as file:
+        with open("ky2.html", "w") as file:
             file.write(str(self.soup))
 
     # add css file
     def Css_file(self):
         head = self.soup.find("head")
+        style = self.soup.head.find("style")
+        style.decompose()
         css_link = self.soup.new_tag("link")
         css_link.attrs[
             "href"] = "https://unicourt.github.io/cic-code-ga/transforms/ga/stylesheet/ga_code_stylesheet.css"
